a914336c1c670f3f7b6b340ca65e17884a47ea0f
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-04-15 18:49:35 -0300

    add modulo 6

---
+++ exercicios/Modulo6-Vetores/ExeR01/ExeR01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        int[] array;
+
+        N = int.Parse(Console.ReadLine());
+        array = new int[N];
+
+        string[] array2 = Console.ReadLine().Split(' ');
+        for (int i = 0; i < N; i++)
+        {
+            array[i] = int.Parse(array2[i]);
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (array[i] < 0)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ exercicios/Modulo6-Vetores/ExeR02/ExeR02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        double[] arr;
+        double soma, media;
+        /*string texto;*/
+
+        N = int.Parse(Console.ReadLine());
+        arr = new double[N];
+
+        string[] arrTemp = Console.ReadLine().Split(' ');
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = double.Parse(arrTemp[i], CultureInfo.InvariantCulture);
+        }
+
+
+        /*texto = "";*/
+        soma = 0;
+        for (int i = 0; i < N; i++)
+        {
+            soma += arr[i];
+            Console.Write(arr[i].ToString("f1", CultureInfo.InvariantCulture) + " ");
+        }
+        Console.WriteLine();
+
+        media = soma / N;
+
+
+        /*Console.WriteLine(texto);*/
+        Console.WriteLine(soma.ToString("f2", CultureInfo.InvariantCulture));
+        Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
+    }
+}
