49bb101185b5fd5e165e3e7719f705b9ebb8e007
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-04-15 12:15:02 -0300

    add modulo 5 estrutura de repetição

---
+++ exercicios/Modulo5-Repeticoes/ExeR01/ExeR01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExeR01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+
+            string[] arr = Console.ReadLine().Split(' ');
+            X = int.Parse(arr[0]);
+            Y = int.Parse(arr[1]);
+
+
+            while (X != Y)
+            {
+                if(X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente");
+                }
+
+                arr = Console.ReadLine().Split(' ');
+                X = int.Parse(arr[0]);
+                Y = int.Parse(arr[1]);
+            }
+
+            Console.WriteLine("FIM");
+        }
+    }
+}
--- exercicios/URI/URI/Program.cs
+++ exercicios/URI/URI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI
 {
@@ -6,23 +7,40 @@ namespace URI
     {
         static void Main(string[] args)
         {
-            int A, B, C, D;
+            double sal, newSal, reajuste, percentual;
 
-            string[] arr = Console.ReadLine().Split(' ');
-            A = int.Parse(arr[0]);
-            B = int.Parse(arr[1]);
-            C = int.Parse(arr[2]);
-            D = int.Parse(arr[3]);
+            Console.Write("Entre com seu salario: ");
+            sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 
-            if(B > C && D > A && C+D > A+B && (C > 0 && D > 0) && A % 2 == 0)
+            if(sal <= 400.00)
             {
-                Console.WriteLine("Valores nao aceitos");
+                percentual = 15.0;
+            }
+            else if(sal <= 800.00)
+            {
+                percentual = 12.0;
+            }
+            else if (sal <= 1200.00)
+            {
+                percentual = 10.0;
+            }
+            else if(sal <= 2000.00)
+            {
+                percentual = 7.0;
             }
             else
             {
-                Console.WriteLine("Valores aceitos");
+                percentual = 4.0;
             }
+
+            newSal = sal * (1.0 + percentual/100);
+            reajuste = newSal - sal;
+
+
+            Console.WriteLine("Novo salario: " + newSal.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + reajuste.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual + " %");
         }
     }
 }
