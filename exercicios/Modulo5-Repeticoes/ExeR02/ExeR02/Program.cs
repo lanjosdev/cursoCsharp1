62a0ceeaab97ff402d3cadfecf9a83b5e92bc0d1
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-04-15 15:30:41 -0300

    Modulo 5 concluido

---
+++ exercicios/Modulo5-Repeticoes/ExeR02/ExeR02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double idade, soma, media, cont;
+
+        idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if(idade < 0)
+        {
+            Console.WriteLine("impossivel calcular");
+        }
+        else
+        {
+            soma = 0;
+            cont = 0;
+
+            while(idade >= 0)
+            {
+                soma += idade;
+                cont++;
+                idade = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+            }
+
+
+            media = soma / cont;
+            Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ exercicios/Modulo5-Repeticoes/ExeR03/ExeR03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+
+        Console.Write("Tabuada do numero: ");
+        N = int.Parse(Console.ReadLine());
+
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i + " x " + N + " = " + (i * N));
+        }
+    }
+}
