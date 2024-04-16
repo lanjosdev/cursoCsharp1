2868160a9d8af849402ed8b3bac40b037c25ed73
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-04-16 16:08:11 -0300

    Modulo 7 finalizado

--- exercicios/Modulo7-Matrizes/ExeR02/ExeR02/Program.cs
+++ exercicios/Modulo7-Matrizes/ExeR02/ExeR02/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int L, N;
+        int[,] matriz;
+        int[] arraySomas;
+
+
+        // INICIO:
+        string[] temp = Console.ReadLine().Split(' ');
+        L = int.Parse(temp[0]);
+        N = int.Parse(temp[1]);
+        matriz = new int[L, N];
+
+        // Leitura/Atribuição na matriz:
+        for (int i = 0; i < L; i++)
+        {
+            temp = Console.ReadLine().Split(' ');
+            for (int j = 0; j < N; j++)
+            {
+                matriz[i, j] = int.Parse(temp[j]);
+            }
+        }
+
+
+        // PROCESSAMENTO:
+        // Criar array com soma de cada linha da matriz:
+        arraySomas = new int[L];
+        int soma;
+        for (int i = 0; i < L; i++)
+        {
+            soma = 0;
+            for (int j = 0; j < N; j++)
+            {
+                soma += matriz[i, j];
+            }
+            arraySomas[i] = soma;
+        }
+
+
+        // SAIDA:
+        Console.WriteLine();
+
+        foreach (int elemento in arraySomas)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+}
